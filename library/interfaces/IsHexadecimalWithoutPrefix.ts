6aa816bad8e255c8c523f4984381cba6f5164438
Chung Nguyen <chungnguyen@anduintransact.com>
2023-09-27 14:48:26 +0700

    feat: add secp256k1 lib

---
+++ library/interfaces/IsHexadecimalWithoutPrefix.ts
@@ -0,0 +1,31 @@
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+import { ValidationArguments } from 'class-validator';
+
+@ValidatorConstraint({
+  name: 'IsHexadecimalWithoutPrefix',
+  async: false,
+})
+export class IsHexadecimalWithoutPrefix
+  implements ValidatorConstraintInterface
+{
+  validate(value: string, args: ValidationArguments) {
+    const hexadecimal = /^[0-9A-Fa-f]+$/i;
+    if (
+      typeof value !== 'string' ||
+      value.length <= 0 ||
+      !hexadecimal.test(value)
+    ) {
+      args.constraints = [{ damem: 'false' }];
+      return false;
+    }
+    return true;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `$property must be a non-empty hexadecimal string`;
+  }
+}
