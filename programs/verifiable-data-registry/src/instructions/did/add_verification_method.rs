42d6724b7e13ba2cb7a48bedf6b289c87587fcd3
vinhtran <tranvinhlx135@gmail.com>
2023-10-03 16:42:57 +0700

    change seed pda

--- programs/verifiable-data-registry/src/instructions/did/add_verification_method.rs
+++ programs/verifiable-data-registry/src/instructions/did/add_verification_method.rs
@@ -1,20 +1,23 @@
 use anchor_lang::prelude::*;
 
 use crate::error::VerifiableDataRegistryError;
-use crate::state::{DidDocument, VerificationMethod};
+use crate::state::*;
 
 #[derive(Accounts)]
-#[instruction(did: String, key_id: String, r#type: String, public_key_multibase: String)]
+#[instruction(did_seed: [u8; 20], verification_seed: [u8; 20], key_id: String, r#type: String, public_key_multibase: String)]
 pub struct AddVerificationMethod<'info> {
     #[account(
         init,
-        seeds=[did.as_bytes(), key_id.as_bytes()],
+        seeds=[verification_seed.as_slice()],
         bump,
         payer = controller,
-        space = 8 + 32 + 4+did.len() + 4+key_id.len() + 4+r#type.len() + 4+public_key_multibase.len()
+        space = 
+            8 + 32 + 4 + VerificationMethod::DISCRIMINATOR.len() 
+            + 4 + did_document.did.len() + 4 + key_id.len() 
+            + 4 + r#type.len() + 4 + public_key_multibase.len()
     )]
     pub verification_method: Account<'info, VerificationMethod>,
-    #[account(seeds=[did.as_bytes()], bump)]
+    #[account(seeds=[did_seed.as_slice()], bump)]
     pub did_document: Account<'info, DidDocument>,
     #[account(mut)]
     pub controller: Signer<'info>,
@@ -23,7 +26,8 @@ pub struct AddVerificationMethod<'info> {
 
 pub fn add_verification_method_handler(
     ctx: Context<AddVerificationMethod>,
-    did: String,
+    _did_seed: [u8; 20],
+    _verification_seed: [u8; 20],
     key_id: String,
     r#type: String,
     public_key_multibase: String,
@@ -33,9 +37,10 @@ pub fn add_verification_method_handler(
         ctx.accounts.did_document.controller == ctx.accounts.controller.key(),
         VerifiableDataRegistryError::Unauthorized,
     );
-    ctx.accounts.verification_method.did = did;
-    ctx.accounts.verification_method.key_id = key_id;
     ctx.accounts.verification_method.controller = controller; // controller of key, not did
+    ctx.accounts.verification_method.discriminator = VerificationMethod::DISCRIMINATOR.to_string();
+    ctx.accounts.verification_method.did = ctx.accounts.did_document.did.clone();
+    ctx.accounts.verification_method.key_id = key_id;
     ctx.accounts.verification_method.r#type = r#type;
     ctx.accounts.verification_method.public_key_multibase = public_key_multibase;
     Ok(())
--- programs/verifiable-data-registry/src/instructions/did/add_verification_relationship.rs
+++ programs/verifiable-data-registry/src/instructions/did/add_verification_relationship.rs
@@ -4,18 +4,22 @@ use crate::error::VerifiableDataRegistryError;
 use crate::state::*;
 
 #[derive(Accounts)]
-#[instruction(did: String, key_id: String)]
+#[instruction(did_seed: [u8; 20], verification_seed: [u8; 20], authentication_seed: [u8; 20])]
 pub struct AddAuthentication<'info> {
     #[account(
         init,
-        seeds=[Authentication::DISCRIMINATOR.as_bytes(), did.as_bytes(), key_id.as_bytes()],
+        seeds=[authentication_seed.as_slice()],
         bump,
         payer = controller,
-        space = 8 + 4+Authentication::DISCRIMINATOR.len() + 4+did.len() + 4+key_id.len()
+        space = 
+            8 + 4 + Authentication::DISCRIMINATOR.len() 
+            + 4 + verification_method.did.len() + 4 + verification_method.key_id.len()
     )]
     pub authentication: Account<'info, Authentication>,
-    #[account(seeds=[did.as_bytes()], bump)]
+    #[account(seeds=[did_seed.as_slice()], bump)]
     pub did_document: Account<'info, DidDocument>,
+    #[account(seeds=[verification_seed.as_slice()], bump)]
+    pub verification_method: Account<'info, VerificationMethod>,
     #[account(mut)]
     pub controller: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -23,32 +27,37 @@ pub struct AddAuthentication<'info> {
 
 pub fn add_authentication_handler(
     ctx: Context<AddAuthentication>,
-    did: String,
-    key_id: String,
+    _did_seed: [u8; 20],
+    _veirifcation_seed: [u8; 20],
+    _authentication_seed: [u8; 20],
 ) -> Result<()> {
     require!(
         ctx.accounts.did_document.controller == ctx.accounts.controller.key(),
         VerifiableDataRegistryError::Unauthorized,
     );
     ctx.accounts.authentication.discriminator = Authentication::DISCRIMINATOR.to_string();
-    ctx.accounts.authentication.did = did;
-    ctx.accounts.authentication.key_id = key_id;
+    ctx.accounts.authentication.did = ctx.accounts.verification_method.did.clone();
+    ctx.accounts.authentication.key_id = ctx.accounts.verification_method.key_id.clone();
     Ok(())
 }
 
 #[derive(Accounts)]
-#[instruction(did: String, key_id: String)]
+#[instruction(did_seed: [u8; 20], verification_seed: [u8; 20], assertion_seed: [u8; 20])]
 pub struct AddAssertion<'info> {
     #[account(
         init,
-        seeds=[Assertion::DISCRIMINATOR.as_bytes(), did.as_bytes(), key_id.as_bytes()],
+        seeds=[assertion_seed.as_slice()],
         bump,
         payer = controller,
-        space = 8 + 4+Assertion::DISCRIMINATOR.len() + 4+did.len() + 4+key_id.len()
+        space = 
+            8 + 4 + Assertion::DISCRIMINATOR.len() 
+            + 4 + verification_method.did.len() + 4 + verification_method.key_id.len()
     )]
     pub assertion: Account<'info, Assertion>,
-    #[account(seeds=[did.as_bytes()], bump)]
+    #[account(seeds=[did_seed.as_slice()], bump)]
     pub did_document: Account<'info, DidDocument>,
+    #[account(seeds=[verification_seed.as_slice()], bump)]
+    pub verification_method: Account<'info, VerificationMethod>,
     #[account(mut)]
     pub controller: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -56,32 +65,37 @@ pub struct AddAssertion<'info> {
 
 pub fn add_assertion_handler(
     ctx: Context<AddAssertion>,
-    did: String,
-    key_id: String,
+    _did_seed: [u8; 20],
+    _veirifcation_seed: [u8; 20],
+    _assertion_seed: [u8; 20],
 ) -> Result<()> {
     require!(
         ctx.accounts.did_document.controller == ctx.accounts.controller.key(),
         VerifiableDataRegistryError::Unauthorized,
     );
     ctx.accounts.assertion.discriminator = Assertion::DISCRIMINATOR.to_string();
-    ctx.accounts.assertion.did = did;
-    ctx.accounts.assertion.key_id = key_id;
+    ctx.accounts.assertion.did = ctx.accounts.verification_method.did.clone();
+    ctx.accounts.assertion.key_id = ctx.accounts.verification_method.key_id.clone();
     Ok(())
 }
 
 #[derive(Accounts)]
-#[instruction(did: String, key_id: String)]
+#[instruction(did_seed: [u8; 20], verification_seed: [u8; 20], key_agreement_seed: [u8; 20])]
 pub struct AddKeyAgreement<'info> {
     #[account(
         init,
-        seeds=[KeyAgreement::DISCRIMINATOR.as_bytes(), did.as_bytes(), key_id.as_bytes()],
+        seeds=[key_agreement_seed.as_slice()],
         bump,
         payer = controller,
-        space = 8 + 4+KeyAgreement::DISCRIMINATOR.len() + 4+did.len() + 4+key_id.len()
+        space = 
+            8 + 4 + KeyAgreement::DISCRIMINATOR.len() 
+            + 4 + verification_method.did.len() + 4 + verification_method.key_id.len()
     )]
     pub key_agreement: Account<'info, KeyAgreement>,
-    #[account(seeds=[did.as_bytes()], bump)]
+    #[account(seeds=[did_seed.as_slice()], bump)]
     pub did_document: Account<'info, DidDocument>,
+    #[account(seeds=[verification_seed.as_slice()], bump)]
+    pub verification_method: Account<'info, VerificationMethod>,
     #[account(mut)]
     pub controller: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -89,15 +103,16 @@ pub struct AddKeyAgreement<'info> {
 
 pub fn add_key_agreement_handler(
     ctx: Context<AddKeyAgreement>,
-    did: String,
-    key_id: String,
+    _did_seed: [u8; 20],
+    _veirifcation_seed: [u8; 20],
+    _key_agreement_seed: [u8; 20],
 ) -> Result<()> {
     require!(
         ctx.accounts.did_document.controller == ctx.accounts.controller.key(),
         VerifiableDataRegistryError::Unauthorized,
     );
     ctx.accounts.key_agreement.discriminator = KeyAgreement::DISCRIMINATOR.to_string();
-    ctx.accounts.key_agreement.did = did;
-    ctx.accounts.key_agreement.key_id = key_id;
+    ctx.accounts.key_agreement.did = ctx.accounts.verification_method.did.clone();
+    ctx.accounts.key_agreement.key_id = ctx.accounts.verification_method.key_id.clone();
     Ok(())
 }
