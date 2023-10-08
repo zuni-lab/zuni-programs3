a4b116e28c92312f0cffe85e54996801d74f56a6
Galin Chung Nguyen <galin.chung.nguyen@gmail.com>
2023-10-08 23:00:44 +0700

    feat: type-safe verifiable credential protocol with single field check proof

---
+++ library/tests/VerifiableCredential.spec.ts
@@ -0,0 +1,214 @@
+import { assert } from 'chai';
+import * as fs from 'fs';
+import { BabyJubCurvePoint } from 'library/BabyJub/BabyJubBasePoint';
+import { FFMathUtility } from 'library/BabyJub/FFMathUtility';
+import { ECCKeyStringPair } from 'library/interfaces/ECCKeyStringPair';
+import { ECCUtility } from 'library/utility/ECCUtility';
+import {
+  PrivateCredential,
+  PublicCredential,
+  Schema,
+  VCPresentation,
+} from 'library/verifiable_credential/VCInterfaces';
+import {
+  createVerificationSchema,
+  decryptPublicCredential,
+  generateVCPresentation,
+  issueVC,
+  verifyValidPublicCredential,
+  verifyValidSchema,
+  verifyVCPresentation,
+} from 'library/verifiable_credential/VCUtility';
+import * as snarkjs from 'snarkjs';
+import { Groth16Proof } from 'snarkjs';
+
+describe('Verifiable Credential protocol', function () {
+  this.timeout(100000);
+  const issuer = 'did:solana:b3d5824fa5c8dcb7dab1ab5951d9fee5';
+  const holder = 'did:solana:b3d5824fa5c8dcb7dab1ab5951d9fee5';
+  const verifier = 'did:solana:b3d5824fa5c8dcb7dab1ab5951d9fee5';
+  let issuerKeys: ECCKeyStringPair<BabyJubCurvePoint>;
+  let holderKeys: ECCKeyStringPair<BabyJubCurvePoint>;
+  let verifierKeys: ECCKeyStringPair<BabyJubCurvePoint>;
+  let schema: Schema<BabyJubCurvePoint>;
+  let publicCredential: PublicCredential<BabyJubCurvePoint>;
+  let fullCredential: PrivateCredential<BabyJubCurvePoint>;
+  let vcpresentation: VCPresentation<BabyJubCurvePoint, Groth16Proof>;
+
+  before(async () => {
+    await FFMathUtility.initialize();
+    // ECCUtility.init('secp256k1');
+    ECCUtility.init('babyjub');
+
+    issuerKeys = ECCUtility.genKeyPair();
+    holderKeys = ECCUtility.genKeyPair();
+    verifierKeys = ECCUtility.genKeyPair();
+  });
+
+  // it('Test ff', async function(){
+  //   const newSMT = await FFMathUtility.createSMT();
+  //   await newSMT.insert(newSMT.F.e(3), newSMT.F.e(20));
+
+  //   const ff = await newSMT.find(newSMT.F.e(3));
+  //   assert(
+  //     FFMathUtility.F.toString(FFMathUtility.F.e(20), 10) ==
+  //       FFMathUtility.F.toString(ff.foundValue, 10),
+  //   );
+  //   const tmp = await getSiblings(newSMT, 3);
+  // })
+
+  it('Issuer issues credential', async function () {
+    // issuer,
+    // holder,
+    // types,
+    // issuerKeyPair,
+    // holderPublicKey,
+    // expirationDate,
+    // credentialSubject,
+    const issuedVCInPrivateForm = await issueVC({
+      issuer: issuer,
+      holder: holder,
+      issuerKeyPair: issuerKeys,
+      holderPublicKey: holderKeys.getPublicKey(),
+      types: ['UniversityDegreeCredential'],
+      expirationDate: new Date('2024-05-20').toISOString(),
+      credentialSubject: JSON.parse(
+        JSON.stringify({
+          degree: {
+            type: 'BachelorDegree',
+            name: 'Bachelor of Science and Arts',
+          },
+          class: 2025,
+          year: 2024,
+          school_name: 'HCMUS',
+          total_credits: 135,
+          num_of_failed_classes: 20,
+        }),
+      ),
+    });
+
+    assert.equal(
+      verifyValidPublicCredential(issuedVCInPrivateForm.toPublicCredential()),
+      true,
+    );
+
+    const invalidPublicCred = issuedVCInPrivateForm
+      .toPublicCredential()
+      .clone();
+    invalidPublicCred.id += ' ';
+    assert.equal(verifyValidPublicCredential(invalidPublicCred), false);
+
+    publicCredential = issuedVCInPrivateForm.toPublicCredential();
+  });
+
+  it('Holder decrypt VC', async function () {
+    fullCredential = decryptPublicCredential(
+      publicCredential,
+      holderKeys.getPrivateKey(),
+    );
+    console.log(fullCredential);
+    assert(fullCredential.credentialSubject !== undefined);
+  });
+
+  it('Verifier creates schema', async function () {
+    schema = await createVerificationSchema({
+      name: 'Computer Science University Degree Check',
+      verifier: verifier,
+      verifierKeyPair: verifierKeys,
+      checks: [
+        JSON.parse(
+          JSON.stringify({
+            degree: {
+              type: ['$EQ', 'BachelorDegree'],
+              name: ['$EQ', 'Bachelor of Science and Arts'],
+            },
+            class: ['$GTE', 2023],
+            year: ['$LT', 19999],
+          }),
+        ),
+        JSON.parse(
+          JSON.stringify({
+            degree: {
+              type: ['$EQ', 'BachelorDegree'],
+              // name: ['$EQ', 'Bachelor of Science and Arts'],
+            },
+            class: ['$GT', 2022],
+            year: ['$LT', 20000],
+          }),
+        ),
+        JSON.parse(
+          JSON.stringify({
+            degree: {
+              type: ['$EQ', 'BachelorDegree'],
+              // name: ['$EQ', 'Bachelor of Science and Arts'],
+            },
+            // class: ['$GTE', 2023],
+            year: ['$LT', 2025],
+          }),
+        ),
+        JSON.parse(
+          JSON.stringify({
+            degree: {
+              type: ['$EQ', 'BachelorDegree'],
+              // name: ['$EQ', 'Bachelor of Science and Arts'],
+            },
+            // class: ['$GTE', 2023],
+            year: ['$LT', 19999],
+            total_credits: ['$GT', 134],
+            num_of_failed_classes: ['$NE', 19],
+          }),
+        ),
+      ],
+      requestedFields: ['0:school_name', '0:school_name', '1:degree.name'],
+    });
+
+    assert.equal(verifyValidSchema(schema), true);
+
+    const invalidSchema = schema.clone();
+    invalidSchema.verifier += ' ';
+    assert.equal(verifyValidSchema(invalidSchema), false);
+  });
+
+  it('Prover create VC presentation for a specific schema', async function () {
+    // schema: Schema<P>,
+    // privateCredentials: Array<PrivateCredential<P>>,
+    // zkEngine: ZKEngine<ZP>,
+    // holder: string,
+    // holderPrivateKey: ECCPrivateKeyInterface<P>,
+    // vcSingleFieldProofWasmPath: string,
+    // vcSingleFieldProofZkeyPath: string,
+
+    vcpresentation = await generateVCPresentation(
+      schema,
+      [fullCredential, fullCredential, fullCredential, fullCredential],
+      snarkjs.groth16,
+      holder,
+      holderKeys.getPrivateKey(),
+      'circuits/vc/vc_single_field/vc_schema_field_check_verifier_js/vc_schema_field_check_verifier.wasm',
+      // 'circuits/vc/vc_single_field/vc_schema_field_check_verifier.wasm',
+      // 'circuits/vc/vc_single_field/vc_schema_field_check_verifier_circuit_final.zkey',
+      'circuits/vc/vc_single_field/vc_schema_field_check_verifier_circuit_final.zkey',
+    );
+  });
+
+  it('Verifier verifies VC presentation', async function () {
+    const vKey = JSON.parse(
+      fs.readFileSync(
+        // 'circuits/vc/vc_single_field/vc_schema_field_check_verifier_verification_key.json',
+        'circuits/vc/vc_single_field/vc_schema_field_check_verifier_verification_key.json',
+        'utf8',
+      ),
+    );
+
+    console.log(vcpresentation);
+
+    const res = await verifyVCPresentation(
+      vcpresentation,
+      snarkjs.groth16,
+      vKey,
+    );
+    assert.equal(res, true);
+
+    process.exit(0);
+  });
+});
